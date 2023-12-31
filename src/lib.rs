6bc862c66b7007bdd6f5fa08aaaca9ee2563541a
Placeless <mikoprpr@hotmail.com>
2023-12-31 13:31:23 +0900

    repair to snakecase

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn Rust2ipynb(_py: Python, m: &PyModule) -> PyResult<()> {
+fn rust2ipynb(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
